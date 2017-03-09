26489f0b790538bc7335595f00a8605835ffa73c
Hajagos <siskeba1@gmail.com>
2017-03-09 21:48:03 +0100

    Server strings added

---
+++ stringconstant.cpp
@@ -0,0 +1,6 @@
+#include "stringconstant.h"
+
+//Date format
+const QString StringConstant::DATE_FORMAT = "yyyy-MM-dd";
+const QString StringConstant::TIME_FORMAT = "hh:mm:ss";
+const QString StringConstant::DATE_TIME_FORMAT = "yyyy-MM-dd hh:mm:ss";
---
+++ stringconstant.h
@@ -0,0 +1,17 @@
+#ifndef STRINGCONSTANT_H
+#define STRINGCONSTANT_H
+
+#include <QString>
+
+class StringConstant
+{
+public:
+
+    //DATE
+    static const QString DATE_FORMAT;
+    static const QString TIME_FORMAT;
+    static const QString DATE_TIME_FORMAT;
+
+};
+
+#endif // STRINGCONSTANT_H
